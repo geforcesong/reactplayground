b09007f2f56b75ced06086f33786a82651f9daf7
geforcesong <georgeguo@outlook.com>
2021-06-11 08:28:25 +0800

    added ninja

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Ninjas from './Ninjas';
 import NinjasFunCompomet from './NinjasFunCompomet';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
@@ -10,13 +11,22 @@ class App extends Component {
       { id: 3, name: 'Andy', age: 59, belt: 'pink' }
     ]
   }
-  
+
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    this.setState({
+      ninjas: [...this.state.ninjas, ninja]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
         <Ninjas ninjas={this.state.ninjas} />
         <NinjasFunCompomet ninjas={this.state.ninjas} />
+
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
--- src/NinjasFunCompomet.js
+++ src/NinjasFunCompomet.js
@@ -1,15 +1,13 @@
-import React from 'react';
-
 const NinjasFunCompomet = (props) => {
     const { ninjas } = props;
     const ninjaList = ninjas.map((ninja) => {
-        return (
+        return ninja.age > 50 ? (
             <div className="ninja" key={ninja.id}>
                 <div>Name: {ninja.name}</div>
                 <div>Age: {ninja.age}</div>
                 <div>Belt: {ninja.belt}</div>
             </div>
-        )
+        ) : null;
     })
 
     return (<div className="ninja">
