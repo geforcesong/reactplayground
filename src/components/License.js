51905070305753503887f4e09dbc0bf2b9ba4ecc
geforcesong <georgeguo@outlook.com>
2022-04-26 09:54:10 +0800

    updated

--- src/components/License.js
+++ src/components/License.js
@@ -1,7 +1,93 @@
-const License = () => {
-    console.log('Reach license');
+import { useRef, useState } from "react";
+import styles from "./License.module.scss";
 
-    return ( <h1>This is License page 1</h1> );
+class Task {
+  constructor(name) {
+    this.id = Math.random();
+    this.name = name;
+    this.status = 0;
+  }
 }
- 
+
+const License = () => {
+  const [tasks, setTasks] = useState([]);
+  const textInput = useRef();
+
+  const handleClick = () => {
+    const task = new Task(textInput.current.value);
+    setTasks((prev) => [...tasks, task]);
+    textInput.current.value = "";
+  };
+
+  const handleTaskChange = (task) => {
+    if (!task.status) {
+      task.status = 1;
+    } else if (task.status == 1) {
+      task.status = 2;
+    } else {
+      task.status = 0;
+    }
+    setTasks((prev) => [...tasks]);
+  };
+
+  function renderTaskRow(task) {
+    if (!task.status) {
+      return (
+        <>
+          <div>{task.name}</div>
+          <div></div>
+          <div></div>
+          <div>
+            <button onClick={() => handleTaskChange(task)}>
+              update status
+            </button>
+          </div>
+        </>
+      );
+    } else if (task.status === 1) {
+      return (
+        <>
+          <div></div>
+          <div>{task.name}</div>
+          <div></div>
+          <div>
+            <button onClick={() => handleTaskChange(task)}>
+              update status
+            </button>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div></div>
+          <div></div>
+          <div>{task.name}</div>
+          <div>
+            <button onClick={() => handleTaskChange(task)}>
+              update status
+            </button>
+          </div>
+        </>
+      );
+    }
+  }
+
+  return (
+    <div>
+      <input type="text" ref={textInput} />
+      <button onClick={handleClick}>Add task</button>
+      <div className={styles.taskHolder}>
+        <div>ToDo</div>
+        <div>Doing</div>
+        <div>Done</div>
+        <div>Buttons</div>
+        {tasks.map((task) => {
+          return renderTaskRow(task);
+        })}
+      </div>
+    </div>
+  );
+};
+
 export default License;
