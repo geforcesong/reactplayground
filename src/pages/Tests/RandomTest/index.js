a8882fbbaeeae41cd2437c509cca7554fe2dc7f1
geforcesong <georgeguo@outlook.com>
2022-04-27 20:20:48 +0800

    dynamic module css

--- src/pages/Tests/RandomTest/index.js
+++ src/pages/Tests/RandomTest/index.js
@@ -1,7 +1,30 @@
-import styles from "./random.module.scss";
+import { useState } from "react";
+import styles, { color0, color1, color2 } from "./random.module.scss";
+
+const textStyles = {
+  0: color0,
+  1: color1,
+  2: color2,
+};
 
 function RandomTest() {
-  return <div className={styles["random-holder"]}>Random Test</div>;
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    if (value < 2) {
+      setValue((prev) => prev + 1);
+    } else {
+      setValue(() => 0);
+    }
+  };
+
+  return (
+    <div className={styles["random-holder"]}>
+      Random Test
+      <div className={textStyles[value]}>Text Color</div>
+      <button onClick={handleClick}>Update</button>
+    </div>
+  );
 }
 
 export default RandomTest;
